be9b194a7dc26c8c5e50942d057e4b58b9f886f9
Melanie Archer <job@twobanjos.com>
2014-09-04 18:40:17 -0700

    Add more time-gathering methods
    * single-responsibility methods for gathering timestamps and comparing to today's date
    * use returned `years` to determine whether to fade a post

--- inject.js
+++ inject.js
@@ -7,9 +7,26 @@ var injected = injected || (function(){
     getTimestamps: function(selector){
       return document.getElementsByClassName(selector);
     },
+    getTimeValue: function(selector, attr){
+      return selector.getAttribute(attr);
+    },
+    isAYearAgo: function(selector){
+    /* obtains the value (string) stored in the title attribute
+       of the span containing .relativetime.
+       Compares that to today's value, then converts to year. */
+      var postDate = new Date(getTimeValue(selector, 'title')), 
+          today = new Date(),
+          difference = today - postDate,
+          tmp = Math.floor(difference / 1000),
+          years = Math.floor(tmp / 31536000);
+      return years;
+    },
+
     fadePosts: function(elements){
       for (var i = 0; i < elements.length; i++){
+        if (isAYearAgo(elements[i]) > 0){
         elements[i].className += ' faded';
+        }
       }
     }
   };
